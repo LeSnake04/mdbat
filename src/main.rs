949d6505546e1f8e600ca5b38a3a687453dca48e
LeSnake04 <dev.lesnake@posteo.de>
2023-11-10 11:42:37 +0100

    display multiple files, --no-color

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,23 @@ use clap::{ArgAction, Parser, Subcommand};
 use termimad::{Area, MadSkin};
 
 #[derive(Parser)]
+#[command(author, version, about, long_about = None, arg_required_else_help = true)]
 struct Cli {
-    file: PathBuf,
+    files: Vec<PathBuf>,
     #[command(subcommand)]
     /// render inside the give bo
     command: Option<CliCommand>,
     #[arg(long, action = ArgAction::SetTrue)]
     /// Optimise colors for black on white terminals
     light: bool,
+    #[arg(long, action = ArgAction::SetTrue)]
+    /// Dont display colors
+    no_color: bool,
 }
 
 #[derive(Subcommand)]
 enum CliCommand {
+    /// Fit content in spezified bounds
     Area {
         left: u16,
         top: u16,
@@ -27,23 +32,25 @@ enum CliCommand {
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
-    let content = read_to_string(&cli.file).context("Failed to read file")?;
     let is_tty = atty::is(atty::Stream::Stdout);
-    let skin = match (cli.light, is_tty) {
+    let skin = match (cli.light, !cli.no_color || is_tty) {
         (false, true) => MadSkin::default_dark(),
         (true, true) => MadSkin::default_light(),
         (_, false) => MadSkin::no_style(),
     };
-    match cli.command {
-        None => println!("{}", skin.term_text(&content)),
-        Some(CliCommand::Area {
-            left,
-            top,
-            width,
-            height,
-        }) => {
-            let area = Area::new(left, top, width, height);
-            println!("{}", skin.area_text(&content, &area));
+    for file in &cli.files {
+        let content = read_to_string(file).context("Failed to read file")?;
+        match cli.command {
+            None => println!("{}", skin.term_text(&content)),
+            Some(CliCommand::Area {
+                left,
+                top,
+                width,
+                height,
+            }) => {
+                let area = Area::new(left, top, width, height);
+                println!("{}", skin.area_text(&content, &area));
+            }
         }
     }
     Ok(())
